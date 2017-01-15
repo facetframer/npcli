b9d971d7e0fab261ccf62b8afd8f4611f225c9a3
Facet <facet@facetframer.com>
2017-01-15 17:32:44 +0000

    npcli -- support multiple data sources

--- npcli/npcli.py
+++ npcli/npcli.py
@@ -45,20 +45,23 @@ def get_names_rec(node):
         return set()
     elif isinstance(node, ast.Tuple):
         return set.union(*map(get_names_rec, node.elts))
+    elif isinstance(node, ast.List):
+        return set.union(*map(get_names_rec, node.elts))
     elif isinstance(node, ast.Slice):
         children = (node.lower, node.step, node.upper)
         return set.union(*map(get_names_rec, [x for x in children if x is not None]))
     else:
         raise ValueError(node)
 
-def uses_data(expr):
+def uses_stdin(expr):
     names = get_names(expr)
     return 'd' in names or 'data' in names
 
 def main():
     parser = argparse.ArgumentParser(description='Interact with numpy from the command line')
-    parser.add_argument('expr', type=str, help='Expression involving d a numpy array')
+    parser.add_argument('expr', type=str, help='Expression involving d, a numpy array')
     parser.add_argument('--debug', action='store_true', help='Print debug output')
+    parser.add_argument('data_sources', type=str, nargs='*', help='Files to read data from. Stored in d1, d2 etc')
 
     parser.add_argument('--input-format', '-I', type=str, help='Dtype of the data read in')
 
@@ -86,14 +89,18 @@ def main():
 
     context = module_dict.copy()
 
-    if uses_data(args.expr):
-        data = read_data(args.input_format)
-        LOGGER.debug('Data length: %s ', len(data))
-        LOGGER.debug('Data shape: %s ', data.shape)
+    if uses_stdin(args.expr):
+        data = read_data(args.input_format, sys.stdin)
         context.update(data=data, d=data)
-        LOGGER.debug('data: %r', data)
     else:
         LOGGER.debug('takes no data')
+
+    for index, source in enumerate(args.data_sources):
+        with open(source) as stream:
+            data = read_data(args.input_format, stream)
+            name1 = 'd{}'.format(index + 1)
+            name2 = 'data{}'.format(index + 1)
+            context.update({name1: data, name2: data})
 
     LOGGER.debug('context: %r', module_dict)
 
@@ -120,15 +127,19 @@ def multiline_eval(expr, context):
     exec(compile(exec_expr, 'file', 'exec'), context) #pylint: disable=exec-used
     return eval(compile(eval_expr, 'file', 'eval'), context) #pylint: disable=eval-used
 
-def read_data(input_format):
+def read_data(input_format, stream):
     if input_format is not None:
-        data = numpy.fromstring(sys.stdin.read(), dtype=input_format)
-        return data
+        data = numpy.fromstring(stream.read(), dtype=input_format)
     else:
-        data = numpy.array([map(float, line.split()) for line in sys.stdin.read().splitlines()])
+        data = numpy.array([map(float, line.split()) for line in stream.read().splitlines()])
         if data.shape[1] == 1:
             # Treat a stream of numbers a 1-D array
-            return data.flatten()
+            data = data.flatten()
+
+    LOGGER.debug('Data length: %s ', len(data))
+    LOGGER.debug('Data shape: %s ', data.shape)
+    LOGGER.debug('data: %r', data)
+    return data
 
 def imp(s):
     name = s.split('.')[0]
